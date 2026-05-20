e8b49899c0955658ad2d506f7267d42913ea61fb
Yutao Fang <happystraw@outlook.com>
2026-05-20 20:11:14 +0800

    refactor(function): use executor_globals.exception instead of EG() macro

--- src/function.zig
+++ src/function.zig
@@ -102,7 +102,7 @@ fn wrapFn(comptime func_desc: [:0]const u8, comptime func: anytype) Fn {
                 else => @compileError(std.fmt.comptimePrint("unsupported function signature for {s}: expected fn(Ctx) or fn()", .{func_desc})),
             };
             _ = @as(anyerror!void, @call(.auto, func, args)) catch |err| {
-                if (c.EG("exception") == null) {
+                if (c.executor_globals.exception == null) {
                     errors.throwError(null, "%s at %s", .{ @errorName(err).ptr, func_desc.ptr });
                 }
             };
@@ -140,7 +140,7 @@ fn wrapMethod(comptime Class: type, comptime func_desc: [:0]const u8, comptime f
                 };
             };
             _ = @as(anyerror!void, @call(.auto, func, args)) catch |err| {
-                if (c.EG("exception") == null) {
+                if (c.executor_globals.exception == null) {
                     errors.throwError(null, "%s at %s", .{ @errorName(err).ptr, func_desc.ptr });
                 }
             };
